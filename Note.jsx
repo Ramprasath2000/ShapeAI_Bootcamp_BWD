a336aba212bf470f31d9405dd6dc69fffdec5cb4
Ramprasath2000 <85419043+Ramprasath2000@users.noreply.github.com>
2021-06-06 04:52:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Note()
+{
+return(
+<div>
+<h1>My view on Pokemon </h1>
+<p>Pokemon is an Anime.  We travel throught the young trainer named
+Ash Ketchem from Palette town.  His aim is to become a pokemon master.</p>
+</div>
+);
+}
+export default Note;
